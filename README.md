b1bb6aa97a8242a659eae2c89ba836ad2543b303
victorrb91 <vbat2011@gmail.com>
2025-08-25 11:52:27 -0300

    atualização do readme

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # PROJETO_SPRINT_5
 Projeto para avaliação da Sprint 5.
+
+Este projeto é um aplicativo web interativo, utilizando a biblioteca Streamlit, para visualizar dados de anúncios de vendas de carros.
+
+O dashboard permite que o usuário explore o conjunto de dados de forma interativa. As funcionalidades incluem:
+
+    1) Gerar um histograma para a quilometragem dos veículos, permitindo entender a distribuição das vendas.
+    2) Gerar um gráfico de dispersão para comparar o preço e a quilometragem, ajudando a identificar a relação entre as duas variáveis.
+
+O aplicativo está implantado publicamente e pode ser acessado através do seguinte link:
+
--- app.py
+++ app.py
@@ -5,3 +5,21 @@ import plotly_express as px
 st.header('Dashboard de Anúncios de Venda de Carros')
 
 car_data = pd.read_csv('vehicles.csv')
+
+hist_button = st.button('Criar histograma de odômetro')
+
+if hist_button:
+    st.write('Criando um histograma para o conjunto de dados de anúncios de carros')
+
+    fig = px.histogram(car_data, x="odometer")
+
+    st.plotly_chart(fig, use_container_width=True)
+
+scatter_button = st.button('Criar gráfico de dispersão')
+
+if scatter_button:
+    st.write('Criando um gráfico de dispersão para o conjunto de dados de anúncios de carros')
+
+    fig = px.scatter(car_data, x="odometer", y="price")
+
+    st.plotly_chart(fig, use_container_width=True)
